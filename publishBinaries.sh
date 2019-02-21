271b2dac001455f75390c0bf6cc52d10717fcc4d
Jan von Pichowski <jan.pichowski@stud-mail.uni-wuerzburg.de>
2019-02-21 21:37:17 +0100

    Added pushback

--- publishBinaries.sh
+++ publishBinaries.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-#git remote set-url origin ${BITBUCKET_GIT_HTTP_ORIGIN}
-#git add project-native/src/binding/resources/native/*
-#git commit -m "[skip ci] Updating native binaries for latest build ${BITBUCKET_BUILD_NUMBER}."
-#git push origin HEAD:master
+#
+# script for continous integration system to push back libraries
+#
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git add project-native/src/binding/resources/native/*
+  git commit --message "[skip ci] Updating native binaries for latest build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-pushback https://${GH_TOKEN}@github.com/jvpichowski/swig-template.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pushback ${TRAVIS_BRANCH}
+}
+
+if ${TRAVIS_PULL_REQUEST} = 'false'; then
+  setup_git
+  commit_website_files
+  upload_files
+fi
