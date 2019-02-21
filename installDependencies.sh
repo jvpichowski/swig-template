0ce8221f251773f7efbc7d72af75d7ec519511b6
Jan von Pichowski <jan.pichowski@stud-mail.uni-wuerzburg.de>
2019-02-21 21:56:19 +0100

    Added buidl options for mac

--- installDependencies.sh
+++ installDependencies.sh
@@ -4,5 +4,13 @@
 # Installs all dependencies for building linux and windows libraries on linux
 #
 
-sudo apt-get update
-sudo apt-get install -y swig cmake build-essential g++-multilib mingw-w64
+if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
+    echo "installing dependencies for linux build"
+    sudo apt-get update
+    sudo apt-get install -y swig cmake build-essential g++-multilib mingw-w64
+elif [ "${TRAVIS_OS_NAME}" == "osx" ]; then
+    echo "installing dependencies for mac build"
+    brew install cmake
+else 
+    echo "target system is not supported: ${TRAVIS_OS_NAME}"
+fi
