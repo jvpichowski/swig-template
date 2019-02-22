7e19522ae7c3706e032990b9072547e273c21108
Jan von Pichowski <jan.pichowski@stud-mail.uni-wuerzburg.de>
2019-02-22 17:13:42 +0100

    Added windows toolchain

---
+++ project-native/toolchains/ubuntu_g++32.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_CXX_FLAGS "-m32" CACHE STRING "" FORCE)
+
+include_directories(/usr/lib/jvm/default-java/include;/usr/lib/jvm/default-java/include/linux;)
---
+++ project-native/toolchains/ubuntu_g++64.cmake
@@ -0,0 +1,3 @@
+set(CMAKE_SYSTEM_NAME Linux)
+
+include_directories(/usr/lib/jvm/default-java/include;/usr/lib/jvm/default-java/include/linux;)
