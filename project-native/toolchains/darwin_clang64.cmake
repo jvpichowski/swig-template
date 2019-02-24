5b4b2980ab89ad4e672c34e169edb18358093ca3
Jan von Pichowski <jan.pichowski@stud-mail.uni-wuerzburg.de>
2019-02-24 16:56:16 +0100

    changed rpath

--- project-native/toolchains/darwin_clang64.cmake
+++ project-native/toolchains/darwin_clang64.cmake
@@ -1,3 +1,18 @@
 set(CMAKE_SYSTEM_NAME Darwin)
 
+
+# use, i.e. don't skip the full RPATH for the build tree
+SET(CMAKE_SKIP_BUILD_RPATH TRUE)
+
+# when building, don't use the install RPATH already
+# (but later on when installing)
+SET(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE) 
+
+# the RPATH to be used when installing
+SET(CMAKE_INSTALL_RPATH "")
+
+# don't add the automatically determined parts of the RPATH
+# which point to directories outside the build tree to the install RPATH
+SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH FALSE)
+
 include_directories(include;include/darwin;)
