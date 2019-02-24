2e9fbfcfe215e8626f097e746ad8010805d88542
Jan von Pichowski <jan.pichowski@stud-mail.uni-wuerzburg.de>
2019-02-25 00:09:56 +0100

    Changed push mode

--- .travis/publishBinaries.sh
+++ .travis/publishBinaries.sh
@@ -10,10 +10,10 @@ setup_git() {
 }
 
 commit_website_files() {
-  git add project-native/src/binding/*
-  git add project-native/src/swig/*
-  git status
-  git commit --message "[skip ci] Updating native binaries from latest travis build: $TRAVIS_JOB_NUMBER"
+  #git add project-native/src/binding/*
+  #git add project-native/src/swig/*
+  #git status
+  #git commit --message "[skip ci] Updating native binaries from latest travis build: $TRAVIS_JOB_NUMBER"
 }
 
 upload_files() {
@@ -21,10 +21,18 @@ upload_files() {
   #git branch travis-binary-deployment 
   #git checkout ${TRAVIS_BRANCH}
   #git merge travis-binary-deployment
-  git pull --no-edit
-  git commit --amend -m "[skip ci] Updating native binaries from latest travis build: $TRAVIS_JOB_NUMBER"
-  git push
+  #git pull --no-edit
+  #git commit --amend -m "[skip ci] Updating native binaries from latest travis build: $TRAVIS_JOB_NUMBER"
+  #git push
   #git push --set-upstream origin-pushback ${TRAVIS_BRANCH}
+
+  echo " - Pushing natives onto branch $TRAVIS_BRANCH"
+  git checkout "$TRAVIS_BRANCH"
+  git add -v project-native/src/binding/*
+  git add -v project-native/src/swig/*
+  git commit -v -m "[ci skip] project: update $TRAVIS_OS_NAME natives"
+  git pull -q --rebase
+  git push git@github.com:jvpichowski/swig-template.git
 }
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_TAG}" == "" ]; then
