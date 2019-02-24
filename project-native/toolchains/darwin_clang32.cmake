74f319b875d1dbe8837ca5ac9723d177fb31a4df
Jan von Pichowski <jan.pichowski@stud-mail.uni-wuerzburg.de>
2019-02-24 16:14:25 +0100

    Added skip of rpath

--- project-native/toolchains/darwin_clang32.cmake
+++ project-native/toolchains/darwin_clang32.cmake
@@ -1,4 +1,5 @@
 set(CMAKE_SYSTEM_NAME Darwin)
 set(CMAKE_CXX_FLAGS "-m32" CACHE STRING "" FORCE)
+set(CMAKE_SKIP_RPATH True)
 
 include_directories(include;include/darwin;)
--- project-native/toolchains/darwin_clang64.cmake
+++ project-native/toolchains/darwin_clang64.cmake
@@ -1,3 +1,4 @@
 set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_SKIP_RPATH True)
 
 include_directories(include;include/darwin;)
